bede4441bf4354fd22c08dac41c124c647e6e665
joshin-run <jahmsc@gmail.com>
2017-06-21 15:33:33 -0600

    Feat (style) Add logo, change header, add content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/flexgrid-pxlogo.jpg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Flex and CSS Grid: Styling with Style</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Before we begin we'd like to ask a couple of <code>coder</code> questions...
         </p>
+        <p>Who has heard of Flexbox? Used it?</p>
+        <p>Who has heard of CSS Grid? Used it?</p>
+        <p>Well, today Brian/Josh and I are going to descripe Flex and Grid. <br />We'll talk about their independent concepts and also how they can work well together. <br />Let's get started with Brian and the world of Flexbox.</p>
       </div>
     );
   }
