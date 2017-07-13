904a56d5f64eff476fb91b0a338d0a1340ff5433
joshin-run <jahmsc@gmail.com>
2017-07-13 12:24:22 -0600

    Feat (main) Create components, css files

---
+++ src/Components/Combo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import '../styles/NavBar.css'
+
+class Combo extends Component {
+  render() {
+    return (
+      <div className="navbar">
+      <h1>Combo</h1>
+      </div>
+    );
+  }
+}
+
+export default Combo;
